659e8a4108b164d77a7eba6722050af475af541a
vmarialuz <vmarialuzm@gmail.com>
2024-01-23 17:16:27 -0500

    Fetch con Hook

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,12 +1,12 @@
 import { ContadorComponent } from "./components/ContadorComponent"
-import { FormularioComponent } from "./components/FormularioComponent"
+import { UsuariosComponent } from "./components/UsuariosComponent"
 
 export const HooksApp = () => {
   return (
     <>
         <h1>Aplicación de Hooks</h1>
         <hr />
-        <FormularioComponent></FormularioComponent>
+        <UsuariosComponent></UsuariosComponent>
     </>
   )
 }
